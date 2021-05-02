91cc69385d0d762be798f50a0c714873eb5befcf
7df <42803044+7df@users.noreply.github.com>
2021-05-02 22:21:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -322,10 +322,16 @@ client.on('message', msg => {
 });
 
 
-
+client.on('message' , message => {
+  var prefix = "$";
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "ping")) {
+ message.channel.send
 
 client.on('message', message => {
-  if (message.content.startsWith('$king')) {
+  var prefix = "$";
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "king")) {
      if(!message.channel.guild) return;
   message.member.addRole(message.guild.roles.find("name", "kings"));
  
